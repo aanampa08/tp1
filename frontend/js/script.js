c1217d0d291e0d7dce809186385c7ff7e1a9e140
marygalindofiuba <mgalindo@fi.uba.ar>
2024-06-29 02:00:47 -0300

    Arregle el swiper en HTML, JS y CSS

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -2,6 +2,22 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const btnAdopcion = document.getElementById("btnAdopcion");
     btnAdopcion.addEventListener("click", (e) => {
         window.location.href = "usuario/index.html";
-
     });
+});
+
+var swiper = new Swiper(".testimonios-slider", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
 });
