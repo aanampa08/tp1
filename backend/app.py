1160fb4715fdec7c39216bca77c11095ad03b3ce
aliianampa <aliianampa2003@gmail.com>
2024-07-13 19:14:45 -0300

    Agrego nuevo endpoint para filtrar animales

--- backend/app.py
+++ backend/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 CORS(app)
 port = 5000
 #Configuracion para la base de datos: HuellitasBA
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://vicksdb:1234@localhost:5432/HuellitasBA'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://aliss:123456@localhost:5432/HuellitasBA'
 
 migrate = Migrate(app, db)
 
@@ -29,6 +29,36 @@ def obtenerTipoAnimal():
         return jsonify({'tiposAnimal': tiposListado})
     except Exception as error:
         return jsonify({'message': f'Internal Server Error: {error}'}), 500
+
+@app.route('/tipoAnimal/<idTipoAnimal>')
+def obtenerPorTipoAnimal(idTipoAnimal):
+    try:
+        tipo = tipoAnimal.query.filter_by(idTipoAnimal=idTipoAnimal).first()
+        animales = Animal.query.where(Animal.tipoAnimalID == tipo.idTipoAnimal).all()
+        animalesListado = []
+        for animal in animales:
+            #Filtro el barrio y el sexo por cada animal para tener la informacion completa
+            barrio = Barrio.query.where(Barrio.idBarrio == animal.barrioID).first()
+            sexo = Sexo.query.where(Sexo.idSexo == animal.sexoID).first()
+            #Ordeno la informacion para mostrar en la API
+            datoAnimal = {
+                'id': animal.idAnimal,
+                'Nombre': animal.Nombre,
+                'Edad': animal.Edad,
+                'Tipo': animal.tipoEdad,
+                'Sexo':sexo.Nombre,
+                'Descripcion': animal.Descripcion,
+                'Foto': animal.Foto,
+                'Contacto': animal.Contacto,
+                'Barrio':barrio.Nombre
+            }
+            #Guardamos cada animal en el listado
+            animalesListado.append(datoAnimal)
+        #Retornamos el json con los animales cargados
+        return jsonify({'animales': animalesListado})
+    except Exception as error:
+        return jsonify({'message': f'Internal Server Error: {error}'}), 500
+
 
 @app.route('/barrios')
 def obtenerBarrios():
