63da3a68f22dc7458f29f0523b9bd67bafd22a30
VicksDb <mdebiase@fi.uba.ar>
2024-07-15 07:00:01 -0300

    Agrego las cards de adopciones a la página

--- frontend/adopcion/js/scriptRequests.js
+++ frontend/adopcion/js/scriptRequests.js
@@ -1,44 +1,111 @@
-
-function response_received(response) {          
-    return response.json()          
+function response_received(data) {
+    response = data.json();
+    return response;
 }
 
-function parse_data(content) {
-       
-    const container = document.getElementById("adoptionRequests")
-    for(let index = 0; index < content.length; index++){
-        const item = document.createElement("div")
-        item.setAttribbute("class", "item")
-        const card = document.createElement("article")
+function parse_data(adopciones) {
+
+    let adopcionesListado = adopciones['adopciones'];
+    let adoptionRequestDiv = document.getElementById("adoptionRequest");
 
 
-        const card_head =  document.createElement("h3")
-        card_head.setAttribute("class", "cardHead")
-        card_head.innerHTML = `Animal a adoptar : ${content[index].Animal} ` 
+    for (let i = 0; i < adopcionesListado.length; i++) {
+        const adopcion = adopcionesListado[i];
 
-        const card_body = document.createElement("div")
-        card_body.setAttribute("class", "card-body")
+        let divCard = document.createElement("div");
+        divCard.className = `card card-${i % 4}`;
 
-
-
-
-        card_body.append(content[index].Nombre)
-        card.append(image)
-        card.append(card_body)
-        item.append(card)
+        //let divCard = document.createElement("div");
+        //divCard.className = `card card-${i % 5}`;
 
 
-        container.append(item)
-    }                  
+        let pExit = document.createElement("p");
+        pExit.className = "card__exit";
+        let iExit = document.createElement("i");
+        iExit.className = "fa-solid fa-xmark"
+
+        let nombreAnimal = document.createElement("h3");
+        nombreAnimal.innerText = `Solicitud para adoptar a ${adopcion.Animal}`;
+        nombreAnimal.className = "card__title"
+
+
+        let nombre = document.createElement("p");
+        nombre.innerHTML = ` <b>Nombre:</b>${adopcion.Nombre}`;
+        nombre.className = "card__title"
+
+
+        let edad = document.createElement("p");
+        edad.innerHTML = `<b>Edad: </b>${adopcion.Edad}`;
+        edad.className = "card__title"
+
+
+        let profesion = document.createElement("p");
+        profesion.innerHTML = `<b>Profesión: </b>${adopcion.Profesion}`;
+        profesion.className = "card__title"
+
+
+        let telefono = document.createElement("p");
+        telefono.innerHTML = `<b>Teléfono: </b>${adopcion.Telefono}`;
+        telefono.className = "card__title"
+
+
+        let email = document.createElement("p");
+        email.innerHTML = `<b>Email: </b>${adopcion.Email}`;
+        email.className = "card__title"
+
+
+        let direccion = document.createElement("p");
+        direccion.innerHTML = `<b>Dirección: </b>${adopcion.Direccion}`;
+        direccion.className = "card__title"
+
+
+        let localidad = document.createElement("p");
+        localidad.innerHTML = `<b>Localidad: </b>${adopcion.Localidad}`;
+        localidad.className = "card__title"
+
+
+        let motivo = document.createElement("p");
+        motivo.innerHTML = `<b>Motivo de adopción: </b>${adopcion.Motivo}`;
+        motivo.className = "card__title"
+
+
+        let tipoVivienda = document.createElement("p");
+        tipoVivienda.innerHTML = `<b>Tipo de vivienda: </b>${adopcion.tipoVivienda}`;
+        tipoVivienda.className = "card__title"
+
+
+        // Append elements to the card
+        divCard.append(nombreAnimal);
+        divCard.append(nombre);
+        divCard.append(edad);
+        divCard.append(profesion);
+        divCard.append(telefono);
+        divCard.append(email);
+        divCard.append(direccion);
+        divCard.append(localidad);
+        divCard.append(motivo);
+        divCard.append(tipoVivienda);
+
+        pExit.append(iExit);
+        divCard.append(pExit);
+        
+
+        // Append the card to the adoption requests container
+        adoptionRequestDiv.append(divCard);
+    }
 }
 
 
+// Function to handle errors in the request
 function request_error(error) {
-    console.log("ERROR")
-    console.log(error)              
+    console.log("Error:", error);
 }
 
-fetch("http://localhost:5000/adopciones")
+
+// Fetch adoption requests from the server
+fetch('http://localhost:5000/adopciones')
     .then(response_received)
     .then(parse_data)
-    .catch(request_error)
+    .catch(request_error);
+
+
